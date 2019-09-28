94b8d098540b43271f59e495255926ffa3071a66
Blagoja Stamatovski <stamatovski.blagoja@stud.uni-obuda.hu>
2019-09-28 11:56:43 +0200

    Add abstract SportEvent class.

---
+++ src/com/epam/training/domain/SportEvent.java
@@ -0,0 +1,9 @@
+package com.epam.training.domain;
+
+import java.time.LocalDateTime;
+
+public abstract class SportEvent {
+    String title;
+    LocalDateTime startDate;
+    LocalDateTime endDate;
+}
