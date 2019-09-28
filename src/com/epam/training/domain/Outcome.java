b8dab2f8abfa8ebe3f6f1f07ac89fb1fc0c035c3
Blagoja Stamatovski <stamatovski.blagoja@stud.uni-obuda.hu>
2019-09-28 17:23:40 +0200

    Add Builder to Outcome class.

--- src/com/epam/training/domain/Outcome.java
+++ src/com/epam/training/domain/Outcome.java
@@ -6,4 +6,36 @@ public class Outcome {
     String description;
     List<OutcomeOdd> outcomeOdds;
     Bet bet;
+
+    public static class Builder {
+        String description;
+        List<OutcomeOdd> outcomeOdds;
+        Bet bet;
+
+        public Builder(String description) {
+            this.description = description;
+        }
+
+        public Builder withOutcomeOdds(List<OutcomeOdd> outcomeOdds) {
+            this.outcomeOdds = outcomeOdds;
+
+            return this;
+        }
+
+        public Builder withBet(Bet bet){
+            this.bet = bet;
+
+            return this;
+        }
+
+        public Outcome build() {
+            Outcome outcome = new Outcome();
+
+            outcome.description = this.description;
+            outcome.outcomeOdds = this.outcomeOdds;
+            outcome.bet = this.bet;
+
+            return outcome;
+        }
+    }
 }
