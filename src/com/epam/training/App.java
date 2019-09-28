84d05e80ca02a8ca39a7fc931c55378fd1009707
Blagoja Stamatovski <stamatovski.blagoja@stud.uni-obuda.hu>
2019-09-28 13:04:35 +0200

    Rename Main class to App.

---
+++ src/com/epam/training/App.java
@@ -0,0 +1,23 @@
+package com.epam.training;
+
+import com.epam.training.service.SportsBettingService;
+import com.epam.training.view.View;
+
+public class App {
+
+    SportsBettingService service;
+    View view;
+
+    public App(SportsBettingService service, View view) {
+        this.service = service;
+        this.view = view;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public void play() {
+
+    }
+}
