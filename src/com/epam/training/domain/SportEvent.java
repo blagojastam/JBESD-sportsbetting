8e647eda9ebc68061ecc6335a81b3311cbb19586
Blagoja Stamatovski <stamatovski.blagoja@stud.uni-obuda.hu>
2019-09-28 12:24:59 +0200

    Add list of Bets in SportEvent class.

--- src/com/epam/training/domain/SportEvent.java
+++ src/com/epam/training/domain/SportEvent.java
@@ -1,10 +1,12 @@
 package com.epam.training.domain;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public abstract class SportEvent {
     String title;
     LocalDateTime startDate;
     LocalDateTime endDate;
     Result result;
+    List<Bet> bets;
 }
