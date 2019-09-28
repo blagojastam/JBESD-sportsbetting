939b8077034a73193d52b01dd502cd86e9be6b3f
Blagoja Stamatovski <stamatovski.blagoja@stud.uni-obuda.hu>
2019-09-28 11:54:07 +0200

    Add OutcomeOdd class.

---
+++ src/com/epam/training/domain/OutcomeOdd.java
@@ -0,0 +1,11 @@
+package com.epam.training.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class OutcomeOdd {
+    BigDecimal value;
+    LocalDateTime validFrom;
+    LocalDateTime validUntil;
+    Wager wager;
+}
