51e9cf369e8f5137b37e0d16b226dbfc7628c838
Blagoja Stamatovski <stamatovski.blagoja@stud.uni-obuda.hu>
2019-11-20 21:48:15 +0100

    Fix maven dependency issues.

--- src/main/java/com/epam/training/AppSpring.java
+++ src/main/java/com/epam/training/AppSpring.java
@@ -1,9 +1,11 @@
 package com.epam.training;
 
 import com.epam.training.config.AppConfig;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootApplication
 public class AppSpring  {
 
     public static void main(String[] args) {
